5f48bb4c98c9db4d7548e5b82629c35fcf8093a1
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-04-11 09:24:34 +0000

    Working reward amount detection

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 from PIL import ImageShow
 import pytesseract
 from pyautogui import moveTo
+import time
 testing = True
 
 #These are settings, change these to your needs
@@ -17,6 +18,7 @@ WARNING, THIS ALLOWS THE SCRIPT TO SPEND STELLAR JADE ON TRAILBLAZE POWER
 #global variables
 AmountCollected = 0
 CurrentState = "Temp"
+image: Image.Image
 
 
 #change this if your screen has different dimensions
@@ -37,13 +39,17 @@ ReplenishTrailblazePowerWidthStart = int(MonitorDefaultWidth/4.8)
 ReplenishTrailblazePowerHeightStart = int(MonitorDefaultHeight/3.6)
 ReplenishTrailblazePowerWidthEnd = int(MonitorDefaultWidth/1.2631578947)
 ReplenishTrailblazePowerHeightEnd = int(MonitorDefaultHeight/1.3846153846)
-replenishTrailblazePowerUseWidthHeight = True
 
 ChallengeCompletedWidthStart = int(MonitorDefaultWidth/4.8)
 ChallengeCompletedHeightStart = int(MonitorDefaultHeight/2.0769230769)
 ChallengeCompletedWidthEnd = int(MonitorDefaultWidth/2.2068965517)
 ChallengeCompletedHeightEnd = int(MonitorDefaultHeight/2.16)
 ChallengeCompletedUseWidthHeight = True
+
+RewardWidthStart = int(MonitorDefaultWidth/5.1891891892)
+RewardHeightStart = int(MonitorDefaultHeight/1.0536585366)
+RewardWidthEnd = int(MonitorDefaultWidth/1.3287197232)
+RewardHeightEnd = int(MonitorDefaultHeight/1.4025974026)
 
 PausePixelX = int(MonitorDefaultWidth/1.0212765957)
 PausePixelY = int(MonitorDefaultHeight/36)
@@ -54,7 +60,9 @@ def update_situation():
 	Updates the Global Variable CurrentState to the current state of the game
 	"""
 	global CurrentState
-	image = Image.open("Example_replenish-trailblaze-power_noreserve.png")
+	global image
+	image = Image.open("Example_ChallengeCompleted.png")
+	#image = Image.open("Example_replenish-trailblaze-power_noreserve.png")
 	#image = Image.open("Example_infight_noauto.jpg")
 	#image = ImageGrab.grab()
 	PausePixel = image.getpixel((PausePixelX,PausePixelY))
@@ -80,41 +88,22 @@ def update_situation():
 	update_situation()
 
 
-def readscreen(situation:str = "default"):
-	"""
-	Reads the screen and returns the image as a PIL image object. situation limits the amount of pixel read onscreen to only the amount of pixels necessary for that situation
-	"""
-	match situation:
-		case "default":
-			x1 = 0
-			y1 = 0
-			x2 = MonitorDefaultWidth 
-			y2 = MonitorDefaultHeight
-		case "ReplenishTrailblazePower":
-			x1 = ReplenishTrailblazePowerWidthStart
-			y1 = ReplenishTrailblazePowerHeightStart
-			x2 = ReplenishTrailblazePowerWidthEnd
-			y2 = ReplenishTrailblazePowerHeightEnd 
-		case "ChallengeCompleted":
-			x1 = ChallengeCompletedWidthStart
-			y1 = ChallengeCompletedHeightStart
-			x2 = ChallengeCompletedWidthEnd
-			y2 = ChallengeCompletedHeightEnd
-		case _:
-			print(f"Situation {situation} not found, using whole screen")
-			x1 = 0
-			y1 = 0
-			x2 = MonitorDefaultWidth
-			y2 = MonitorDefaultHeight
-	if testing:
-		return Image.open("Example_replenish-trailblaze-power_reserve.jpg")
-	#ImageShow.show(ImageGrab.grab(bbox=(x1,y1,x2,y2)))
-	return (ImageGrab.grab(bbox = (x1,y1,x2,y2)))
+def update_rewardcount():
+	global AmountCollected
+	global AmountToCollect
+	global image
+	#image = ImageGrab.grab(bbox = (RewardWidthStart,RewardHeightStart,RewardWidthEnd,RewardHeightEnd))
+	image = Image.open("Example_Rewards_norelics3.png")
+	#AmountCollected += 
+	print(pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")[1])
 
 
 #AmountToCollect = int(input("Specify amount of ressources to collect (use 0 for infinite or relics)"))
-update_situation()
-print(CurrentState)
 while True:
-	pass
-#print(pytesseract.image_to_string(readscreen("ReplenishTrailblazePower")))
+	update_situation()
+	print(CurrentState)
+	if CurrentState == "InCombat":
+		time.sleep(5)
+		continue
+	if CurrentState == "ChallengeCompleted":
+		update_rewardcount()

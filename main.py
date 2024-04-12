d7c993007f042437ac3d3439c59cffb7c07a937a
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-04-12 13:41:16 +0000

    removed redefinitions

--- main.py
+++ main.py
@@ -7,18 +7,6 @@ import pyautogui
 import time
 testing = True
 
-#These are settings, change these to your needs
-UseReserve = True
-UseFuel = False
-UseStellarJade = False 
-"""
-WARNING, THIS ALLOWS THE SCRIPT TO SPEND STELLAR JADE ON TRAILBLAZE POWER
-"""
-ExitGameAfterCompletion = False
-"""
-If set to true the script closes the whole game after it is done collecting the desired rewards
-"""
-
 
 #global variables
 AmountCollected = 0

231ae3d07ca963c7da207a09cdeea78cb7d086ab
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-07-03 08:14:44 +0000

    AmountToCollect gets set in settings, some formatting

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import numpy as np
 import cv2 as cv
 import pytesseract
 import pyautogui
-import time
+from time import sleep as delay
 
 
 #global variables
 AmountCollected = 0
+AmountToCollect:int
 CurrentState = "Temp"
 image: Image.Image
 FailCounter:int = 0
@@ -63,128 +64,132 @@ FirstItemShapeTwoRows:tuple = 634, 502, 659, 612
 FirstItemShapeOneRow:tuple = 848, 496, 867, 579
 
 def set_settings():
-	global SettingsData
-	global UseReserve
-	global UseFuel
-	global UseStellarJade
-	global ExitGameAfterCompletion
-	global mode
-	global CharacterToIgnore
-	print(SettingsData["AllowedRessources"])
-	for i in SettingsData["AllowedRessources"][len(SettingsData["AllowedRessources"])-1]:
-		match i:
-			case 0:
-				UseReserve=True
-			case 1:
-				UseFuel=True
-			case 2:
-				UseStellarJade=True
-			case _:
-				raise Exception("Allowed ressources has an invalid set ressource")
-	ExitGameAfterCompletion=SettingsData["ExitAfterCompletion"]
-	mode = SettingsData["ChallengeType"][len(SettingsData["ChallengeType"])-1]
-	CharacterToIgnore = SettingsData["IgnoreChar"]
+    global SettingsData
+    global UseReserve
+    global UseFuel
+    global UseStellarJade
+    global ExitGameAfterCompletion
+    global mode
+    global CharacterToIgnore
+    global AmountToCollect
+    print(SettingsData["AllowedRessources"])
+    for i in SettingsData["AllowedRessources"][len(SettingsData["AllowedRessources"])-1]:
+        match i:
+            case 0:
+                UseReserve=True
+            case 1:
+                UseFuel=True
+            case 2:
+                UseStellarJade=True
+            case _:
+                raise Exception("Allowed ressources has an invalid set ressource")
+    ExitGameAfterCompletion=SettingsData["ExitAfterCompletion"]
+    mode = SettingsData["ChallengeType"][len(SettingsData["ChallengeType"])-1]
+    CharacterToIgnore = SettingsData["IgnoreChar"]
+    if SettingsData["AmountToCollect"] == "":
+        raise Exeption("Amount to collect setting is empty")
+    AmountToCollect = int(SettingsData["AmountToCollect"])
  
-		
+        
 
 def update_situation():
-	"""
-	Updates the Global Variable CurrentState to the current state of the game
-	"""
-	global CurrentState
-	global image
-	image = ImageGrab.grab()
-	PausePixel = image.getpixel((PausePixelX,PausePixelY))
-	AddedValue = 0
-	ImageString = pytesseract.image_to_string(image, lang='eng')
-	if "Challenge Completed" in ImageString:
-		CurrentState = "ChallengeCompleted"
-		return
-	if "Replenish Trailblaze Power" in ImageString:
-		CurrentState = "ReplenishTrailblazePower"
-		return
-	if "downed" in ImageString:
-		CurrentState = "DownedChar"
-		return
-	for i in PausePixel:
-		AddedValue += i
-	if AddedValue >= 580:
-		CurrentState = "InCombat"
-		return
-	print("Unknown State; Trying to find state again")
-	update_situation()
+    """
+    Updates the Global Variable CurrentState to the current state of the game
+    """
+    global CurrentState
+    global image
+    image = ImageGrab.grab()
+    PausePixel = image.getpixel((PausePixelX,PausePixelY))
+    AddedValue = 0
+    ImageString = pytesseract.image_to_string(image, lang='eng')
+    if "Challenge Completed" in ImageString:
+        CurrentState = "ChallengeCompleted"
+        return
+    if "Replenish Trailblaze Power" in ImageString:
+        CurrentState = "ReplenishTrailblazePower"
+        return
+    if "downed" in ImageString:
+        CurrentState = "DownedChar"
+        return
+    for i in PausePixel:
+        AddedValue += i
+    if AddedValue >= 580:
+        CurrentState = "InCombat"
+        return
+    print("Unknown State; Trying to find state again")
+    update_situation()
 
 
 def close_game():
-	pyautogui.keyDown('alt')
-	pyautogui.press('f4')
-	pyautogui.keyUp('alt')
+    pyautogui.keyDown('alt')
+    pyautogui.press('f4')
+    pyautogui.keyUp('alt')
 
 
 def mse(imageA, imageB): #https://github.com/CelestialCrafter/hsr-auto-auto-battle/blob/master/main.py#L3
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
-	print(err)
-	return err
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    print(err)
+    return err
 
 
 def determine_rarity(imageA):
-	FourStar = cv.imread("Comparison-Examples/Example_Item-4Star.png")
-	ThreeStar = cv.imread("Comparison-Examples/Example_Item-3Star.png")
-	if mse(imageA, FourStar) > mse(imageA, ThreeStar):
-		return 3
-	else:
-		return 4
+    FourStar = cv.imread("Comparison-Examples/Example_Item-4Star.png")
+    ThreeStar = cv.imread("Comparison-Examples/Example_Item-3Star.png")
+    if mse(imageA, FourStar) > mse(imageA, ThreeStar):
+        return 3
+    else:
+        return 4
 
 
 def update_rewardcount():
-	TwoRows:bool
-	global AmountCollected
-	global AmountToCollect
-	global image
-	FailCountMax:int = 5
-	image = ImageGrab.grab(bbox=RewardOneShape)
-	global FailCounter
-	try:
-		print(int(pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")[1]))
-		TwoRows = False
-	except:
-		image = ImageGrab.grab(bbox=RewardTwoShape)
-		try:
-			print(int(pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")[1]))
-			TwoRows = True
-		except:
-			FailCounter += 1
-			if FailCounter >= FailCountMax:
-				print(f"failed {FailCounter} times which is over max amount ({FailCountMax}), terminating...")
-				raise IndexError
-			print(f"somehow didn't find a reward for single and double row, trying again {FailCounter}/{FailCountMax}")
-			update_rewardcount()
-			return()
-	rewards:list = pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")
-	match mode:
-		case 0:
-			if TwoRows:
-				image = ImageGrab.grab(bbox = FirstItemShapeTwoRows)
-			else:
-				image = ImageGrab.grab(bbox = FirstItemShapeOneRow)
-			image.save("temp/screenshot_temp.png")
-			cvimage = cv.imread("temp/screenshot_temp.png")
-			rarity = determine_rarity(cvimage)
-			match rarity:
-				case 4:
-					AmountCollected += int(rewards[1]) + float(int(rewards[2])/3) + float(int(rewards[3])/9)
-				case 3:
-					AmountCollected += float(int(rewards[1])/3) + float(int(rewards[2])/9)
-				case _:
-					raise Exception("Unknown Rarity, made it into calculation")
-		case 1:
-			AmountCollected += rewards[1]
-		case 2:
-			AmountCollected += 1
-		case _:
-			raise Exception("invalid mode")
-			
+    TwoRows:bool
+    global AmountCollected
+    global AmountToCollect
+    global image
+    FailCountMax:int = 5
+    image = ImageGrab.grab(bbox=RewardOneShape)
+    global FailCounter
+    try:
+        print(int(pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")[1]))
+        TwoRows = False
+    except:
+        image = ImageGrab.grab(bbox=RewardTwoShape)
+        try:
+            print(int(pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")[1]))
+            TwoRows = True
+        except:
+            FailCounter += 1
+            if FailCounter >= FailCountMax:
+                print(f"failed {FailCounter} times which is over max amount ({FailCountMax}), terminating...")
+                raise IndexError
+            print(f"somehow didn't find a reward for single and double row, trying again {FailCounter}/{FailCountMax}")
+            update_rewardcount()
+            return()
+    rewards:list = pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")
+    match mode:
+        case 0:
+            if TwoRows:
+                image = ImageGrab.grab(bbox = FirstItemShapeTwoRows)
+            else:
+                image = ImageGrab.grab(bbox = FirstItemShapeOneRow)
+            image.save("temp/screenshot_temp.png")
+            cvimage = cv.imread("temp/screenshot_temp.png")
+            rarity = determine_rarity(cvimage)
+            match rarity:
+                case 4:
+                    AmountCollected += int(rewards[1]) + float(int(rewards[2])/3) + float(int(rewards[3])/9)
+                case 3:
+                    AmountCollected += float(int(rewards[1])/3) + float(int(rewards[2])/9)
+                case _:
+                    raise Exception("Unknown Rarity, made it into calculation")
+        case 1:
+            AmountCollected += rewards[1]
+        case 2:
+            AmountCollected += 1
+        case _:
+            raise Exception("invalid mode")
+            
 
 # def heal():
 # 		pyautogui.press('m')
@@ -198,151 +203,149 @@ def update_rewardcount():
 
 
 def farm():
-	if mode == 2:
-		AmountToCollect = int(input("Specify amount of cycles\n"))
-	else:
-		AmountToCollect = int(input("Specify amount of ressources to collect\n"))
-	time.sleep(3)
-	while True:
-		update_situation()
-		print(CurrentState)
-		match CurrentState:
-			case "InCombat":
-				time.sleep(5)
-				continue
-			case "ChallengeCompleted":
-				if not SkipRewardCount:
-					update_rewardcount()
-				if AmountCollected >= AmountToCollect:
-					print(f"Collected enough ressources ({(int(AmountCollected*100))/100}), terminating...")
-					if ExitGameAfterCompletion:
-						close_game()
-						break
-					time.sleep(0.5)
-					pyautogui.moveTo(x= StopButtonX,y= BothButtonY)
-					pyautogui.click()
-					break
-				else:
-					print(f"reached {(int(AmountCollected*100))/100}/{AmountToCollect} ressources, starting again...")
-					pyautogui.moveTo(AgainButtonX,BothButtonY)
-					pyautogui.click()
-					time.sleep(1)
-					continue
-			case "ReplenishTrailblazePower":
-				pyautogui.moveTo(1180,735)
-				ReplenishString = pytesseract.image_to_string(image, 'eng')
-				if "Reserved" in ReplenishString and UseReserve:
-					print("Using reserve to replenish Trailblaze Power")
-					pyautogui.click()
-					pyautogui.moveTo(1185,795)
-				elif "Exchange" in ReplenishString and UseFuel:
-					pyautogui.click()
-					print("Using fuel to replenish Trailblaze Power")
-				elif "Consume" in ReplenishString and UseStellarJade:
-					print("Using Stellar Jade to replenish Trailblaze Power")
-				else:
-					print(f"Can't use any replenishment, reached {(int(AmountCollected*100))/100}/{AmountToCollect}, ending...")
-					if ExitGameAfterCompletion:
-						close_game()
-						break
-					pyautogui.moveTo(760,735)
-					pyautogui.click()
-					pyautogui.moveTo(StopButtonX, BothButtonY)
-					pyautogui.click()
-					break
-				pyautogui.click()
-				time.sleep(0.75)
-				pyautogui.click()
-				time.sleep(0.75)
-				pyautogui.moveTo(AgainButtonX, BothButtonY)
-				pyautogui.click()
-				time.sleep(0.75)
-				continue
-			case "DownedChar":
-				image = ImageGrab.grab(bbox=Downtext)
-				CharName = pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")[0]
-				print(f"{CharName} is currently downed, ")
-				if CharName==CharacterToIgnore:
-					print("ignoring...")
-					pyautogui.click(1185,679)
-					continue
-				else:
-					print("stopping...")
-					pyautogui.moveTo(764,675)
-					pyautogui.click()
-					time.sleep(0.2)
-					pyautogui.moveTo(StopButtonX, BothButtonY)
-					pyautogui.click()
-					time.sleep(2)
-					if ExitGameAfterCompletion:
-						close_game()
-					break
+    if mode == 2:
+        AmountToCollect = int(input("Specify amount of cycles\n"))
+    else:
+        AmountToCollect = int(input("Specify amount of ressources to collect\n"))
+    delay(3)
+    while True:
+        update_situation()
+        print(CurrentState)
+        match CurrentState:
+            case "InCombat":
+                delay(3)
+                continue
+            case "ChallengeCompleted":
+                if not SkipRewardCount:
+                    update_rewardcount()
+                if AmountCollected >= AmountToCollect:
+                    print(f"Collected enough ressources ({(int(AmountCollected*100))/100}), terminating...")
+                    if ExitGameAfterCompletion:
+                        close_game()
+                        break
+                    delay(0.5)
+                    pyautogui.moveTo(x= StopButtonX,y= BothButtonY)
+                    pyautogui.click()
+                    break
+                else:
+                    print(f"reached {(int(AmountCollected*100))/100}/{AmountToCollect} ressources, starting again...")
+                    pyautogui.moveTo(AgainButtonX,BothButtonY)
+                    pyautogui.click()
+                    delay(1)
+                    continue
+            case "ReplenishTrailblazePower":
+                pyautogui.moveTo(1180,735)
+                ReplenishString = pytesseract.image_to_string(image, 'eng')
+                if "Reserved" in ReplenishString and UseReserve:
+                    print("Using reserve to replenish Trailblaze Power")
+                    pyautogui.click()
+                    pyautogui.moveTo(1185,795)
+                elif "Exchange" in ReplenishString and UseFuel:
+                    pyautogui.click()
+                    print("Using fuel to replenish Trailblaze Power")
+                elif "Consume" in ReplenishString and UseStellarJade:
+                    print("Using Stellar Jade to replenish Trailblaze Power")
+                else:
+                    print(f"Can't use any replenishment, reached {(int(AmountCollected*100))/100}/{AmountToCollect}, ending...")
+                    if ExitGameAfterCompletion:
+                        close_game()
+                        break
+                    pyautogui.click(760,735)
+                    delay(0.75)
+                    pyautogui.click(StopButtonX,BothButtonY)
+                    break
+                pyautogui.click()
+                delay(0.75)
+                pyautogui.click()
+                delay(0.75)
+                pyautogui.click(AgainButtonX,BothButtonY)
+                delay(0.75)
+                continue
+            case "DownedChar":
+                image = ImageGrab.grab(bbox=Downtext)
+                CharName = pytesseract.image_to_string(image, lang='eng', config='--psm 6').split(" ")[0]
+                print(f"{CharName} is currently downed, ")
+                if CharName==CharacterToIgnore:
+                    print("ignoring...")
+                    pyautogui.click(1185,679)
+                    continue
+                else:
+                    print("stopping...")
+                    pyautogui.click(764,675)
+                    delay(0.75)
+                    pyautogui.click(StopButtonX, BothButtonY)
+                    delay(2)
+                    if ExitGameAfterCompletion:
+                        close_game()
+                    break
 
 
-def main(): #https://www.geeksforgeeks.org/create-settings-menu-in-python-pygame/
-	type =     [("materials", "materials"), 
-				("weekly", "weekly"), 
-				("relics", "relics")] 
+def GUI(): #https://www.geeksforgeeks.org/create-settings-menu-in-python-pygame/
+    type =     [("materials", "materials"), 
+                ("weekly", "weekly"), 
+                ("relics", "relics")] 
  
-	ressources = [("Reserve", "resereve"),
-				  ("Fuel", "fuel"),
-				  ("Stellar-Jade", "stellar-jade")]
+    ressources = [("Reserve", "resereve"),
+                    ("Fuel", "fuel"),
+                    ("Stellar-Jade", "stellar-jade")]
 
-	def setSettings():
-		global SettingsData
-		SettingsData = settings.get_input_data()
+    def setSettings():
+        global SettingsData
+        SettingsData = settings.get_input_data()
 
-	def start():
-		pygame.quit()
-		setSettings()
-		set_settings()
-		farm()
-		exit()
+    def start():
+        setSettings()
+        pygame.quit()
+        set_settings()
+        farm()
+        exit()
 
-	settings = pm.Menu(title="Settings", 
-					width=WIDTH, 
-					height=HEIGHT, 
-					theme=pm.themes.THEME_GREEN) 
+    settings = pm.Menu(title="Settings", 
+                    width=WIDTH, 
+                    height=HEIGHT, 
+                    theme=pm.themes.THEME_GREEN) 
 
-	settings._theme.widget_font_size = 25
-	settings._theme.widget_font_color = BLACK 
-	settings._theme.widget_alignment = pm.locals.ALIGN_LEFT 
+    settings._theme.widget_font_size = 25
+    settings._theme.widget_font_color = BLACK 
+    settings._theme.widget_alignment = pm.locals.ALIGN_LEFT 
 
-	settings.add.dropselect(title="Challenge type", items=type, 
-							dropselect_id="ChallengeType", default=0,selection_box_height=4) 
-	settings.add.dropselect_multiple(title="Allowed Replenishment-ressources", items=ressources,
-									dropselect_multiple_id="AllowedRessources",
-									open_middle=True, max_selected=0, default=0,
-									selection_box_height=4)
+    settings.add.dropselect(title="Challenge type", items=type, 
+                            dropselect_id="ChallengeType", default=0,selection_box_height=4) 
+    settings.add.dropselect_multiple(title="Allowed Replenishment-ressources", items=ressources,
+                                    dropselect_multiple_id="AllowedRessources",
+                                    open_middle=True, max_selected=0, default=0,
+                                    selection_box_height=4)
  
-	settings.add.text_input(title="Character to ignore when down (case sensitive) :", textinput_id="IgnoreChar") 
+    settings.add.text_input(title="Character to ignore when down (case sensitive) :", textinput_id="IgnoreChar") 
  
-	settings.add.toggle_switch( 
-		title="Close game when  done", default=False, toggleswitch_id="ExitAfterCompletion") 
+    settings.add.toggle_switch( 
+        title="Close game when  done", default=False, toggleswitch_id="ExitAfterCompletion") 
 
-	settings.add.button(title="Restore Defaults", action=settings.reset_value, 
-						font_color=WHITE, background_color=RED) 
-	settings.add.button(title="Return To Main Menu", 
-						action=pm.events.BACK, align=pm.locals.ALIGN_CENTER) 
+    settings.add.text_input(title="Amount of ressources to farm/waves to complete :", textinput_id="AmountToCollect") 
 
-	mainMenu = pm.Menu(title="Main Menu", 
-					width=WIDTH, 
-					height=HEIGHT, 
-					theme=pm.themes.THEME_GREEN) 
+    settings.add.button(title="Restore Defaults", action=settings.reset_value, 
+                        font_color=WHITE, background_color=RED) 
+    settings.add.button(title="Return To Main Menu", 
+                        action=pm.events.BACK, align=pm.locals.ALIGN_CENTER) 
 
-	mainMenu._theme.widget_alignment = pm.locals.ALIGN_CENTER 
+    mainMenu = pm.Menu(title="Main Menu", 
+                    width=WIDTH, 
+                    height=HEIGHT, 
+                    theme=pm.themes.THEME_GREEN) 
 
-	pygame.display.set_caption("HSR farming script")
+    mainMenu._theme.widget_alignment = pm.locals.ALIGN_CENTER 
 
-	mainMenu.add.button(title="Settings", action=settings, 
-						font_color=WHITE, background_color=GREEN) 
+    pygame.display.set_caption("HSR farming script")
 
-	mainMenu.add.label(title="") 
+    mainMenu.add.button(title="Settings", action=settings, 
+                        font_color=WHITE, background_color=GREEN) 
 
-	mainMenu.add.button(title="Start", action=start, 
-						font_color=WHITE, background_color=RED) 
-	mainMenu.mainloop(screen) 
+    mainMenu.add.label(title="Please check settings if this is your first time using this script") 
+
+    mainMenu.add.button(title="Start", action=start, 
+                        font_color=WHITE, background_color=RED) 
+    mainMenu.mainloop(screen) 
 
  
 if __name__ == "__main__":
-	main()
+    GUI()
